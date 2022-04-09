4d49a267357930dfd02ec04febd4578dbd34f20c
mperina11 <mperina173@gmail.com>
2022-04-08 19:04:42 -0700

    explosion animation done

--- prefabs/Rocket.js
+++ prefabs/Rocket.js
@@ -35,4 +35,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }   
     }
 
+    // reset rocket to 'ground'
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+
   }
--- prefabs/Spaceship.js
+++ prefabs/Spaceship.js
@@ -9,7 +9,11 @@ class Spaceship extends Phaser.GameObjects.Sprite {
     update () {
         this.x -= this.moveSpeed;
         if (this.x <= 0 - this.width) {
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
